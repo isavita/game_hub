78dd70cbe76573088e28488405f561f05a64da33
Aleksandar Dimov <isavitaisa@gmail.com>
2015-03-23 16:47:30 +0200

    Add a country code field to user for country_select gem.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ActiveRecord::Base
 
   has_attached_file :avatar, styles: { medium: '250x250>', thumb: '100x100>' }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  validates_attachment_size :avatar, less_than: 5.megabytes
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150303212624) do
+ActiveRecord::Schema.define(version: 20150323143940) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20150303212624) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "country_code"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
